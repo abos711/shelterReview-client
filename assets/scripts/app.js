c229ef0f2464c1022a2f25f6b8b293761378446b
Alexander Bos <abos@revridge.com>
2020-11-06 11:50:43 -0500

    11/6 Client side commit
    CLIENT successful with API

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,9 +3,19 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
+const events = require('./auth/events')
+// const listing = require('./listing')
+
 // use require without a reference to ensure a file is bundled
 // require('./example')
+//
+// require('./listing')
 
 $(() => {
-  // your JS code goes here
+  $('#change-password-user-form').hide()
+  $('#sign-out-user-form').hide()
+  $('#sign-up-user-form').on('submit', events.onSignUp)
+  $('#sign-in-user-form').on('submit', events.onSignIn)
+  $('#change-password-user-form').on('submit', events.onChangePassword)
+  $('#sign-out-user-form').on('submit', events.onSignOut)
 })
